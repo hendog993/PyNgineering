d4956e0d0c1e50a2231303124ed8197f92268d5e
Henry V. Gilbert <henrygilbert609@gmail.com>
2019-10-20 13:37:15 -0400

    Update num_int.py

--- num_int.py
+++ num_int.py
@@ -1,27 +1,20 @@
-# Import statements
 from matplotlib import pyplot as plt
-from scipy.integrate import trapz, simps, cumtrapz
-from math import sin, cos, pi, log, exp
-from random import random
 from numpy import linspace
+from math import log, pi, sin
+from scipy.integrate import simps
+from random import random
 
 t = linspace(0.001, 2*pi, 1000)
 y = []
 
-def integrate(lim_a, lim_b):
-	func = lambda x: x * log(x) - x
-	return func(lim_b) - func(lim_a)
-	
-	
+
 for x in t:
-	var = 3*pow(x, 3) + 100*(random() * random())
+	var = sin(x) + (random() * random())
 	y.append(var)
-	
-area_trapz = trapz(y, t)
-area_simps = simps(y, t)
-print("Area trapezoidal rule: {}".format(area_trapz))
-print("Area Simpson's Rule: {}" .format(area_simps))
-# print("Area FTC1: {}".format(integrate(0.001, 2*pi)))
+
+area = simps(y, t)
+print(area)
+
 
 plt.plot(t, y)
 plt.show()
