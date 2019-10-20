1d18c93bb8f69a1341f1a638fe81c3aea92aea9b
Henry V. Gilbert <henrygilbert609@gmail.com>
2019-10-20 09:39:33 -0400

    Update num_diff.py

--- num_diff.py
+++ num_diff.py
@@ -1,24 +1,23 @@
-from numpy import linspace, gradient
 from matplotlib import pyplot as plt
-from math import sin, pi, cos
+from math import sin, cos, pi
+from numpy import gradient, linspace
 
 t = linspace(0, 2*pi, 1000)
 y = []
 
+# This is the function input
 for x in t:
-	# Input your function here.
-	var = sin(x)
+	var = sin(x) * cos(x)
 	y.append(var)
 
 dy = gradient(y)
 dt = gradient(t)
 dydt = dy/dt
 
-plt.plot(t,y, label='Function')
+plt.plot(t,y, label="Function")
 plt.grid()
 plt.plot(t, dydt, label="Derivative")
-plt.xlabel(" t (radians)")
-plt.ylabel(" Sin(t)")
+plt.xlabel("t (radians)")
+plt.ylabel("y")
 plt.legend()
 plt.show()
-
